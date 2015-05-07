1872d4af2b6951aef289293f096d6ee107224c8f
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-05-06 18:43:51 -0700

    Initial Commit

---
+++ AdventureGame/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AdventureGame
+//
+//  Created by Vineet Tiwari on 2015-05-06.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
